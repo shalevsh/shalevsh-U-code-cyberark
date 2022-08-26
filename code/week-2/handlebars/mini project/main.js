8b5d108698fa2b59a1fcd131ee5587678c73d686
shalev <shalevsharf@gmail.com>
2022-08-26 15:54:48 +0300

    mini project upload

---
+++ code/week-2/handlebars/mini project/main.js
@@ -0,0 +1,30 @@
+$("button").on("click", function () {
+    let address = $("#addr-input").val();
+    let minPrice = $("#min-p-input").val();
+    let maxPrice = $("#max-p-input").val();
+    let minRooms = $("#min-r-input").val();
+    let maxRooms = $("#max-r-input").val();
+    let immediate = $("#immed-y");
+
+    let relevantApts = findRelevantApts(
+        address,
+        minPrice,
+        maxPrice,
+        minRooms,
+        maxRooms,
+        immediate
+    );
+    renderApts(relevantApts);
+});
+
+const renderApts = function (apartments) {
+    $("#results").empty();
+    console.log(apartments);
+    const source = $("#apartment-template").html();
+    const template = Handlebars.compile(source);
+    const newHTML = template({ apartments: apartments });
+    console.log(newHTML);
+    $("#results").append(newHTML);
+};
+
+renderApts(apartments); //renders apartments when page loads
