d89aa11cc6ac435a24d557418210074878c52f47
shalev <shalevsharf@gmail.com>
2022-09-04 16:20:10 +0300

    ex2

---
+++ code/week-4/Union Types/exercise-2/main.js
@@ -0,0 +1,16 @@
+"use strict";
+function print_numbers(numbers) {
+    if (Array.isArray(numbers)) {
+        console.log("Array: " + numbers);
+    }
+    else if (typeof numbers === 'string') {
+        console.log('string: ' + numbers);
+    }
+    else if (typeof numbers === 'number') {
+        console.log('number: ' + numbers);
+    }
+}
+print_numbers(972528684411);
+print_numbers([972528684411, 9725242223]);
+print_numbers("052863423243");
+print_numbers(["052343434286", "0547734343"]);
