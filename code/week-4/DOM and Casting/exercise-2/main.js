0425f7706167821bd1bf08d2801a5b5d85137b8e
shalev <shalevsharf@gmail.com>
2022-09-05 15:13:13 +0300

    ex2

--- code/week-4/DOM and Casting/exercise-2/main.js
+++ code/week-4/DOM and Casting/exercise-2/main.js
@@ -1,36 +1,8 @@
 "use strict";
-class Numbers {
-    constructor(numbers) {
-        this.arrayOfNumbers = numbers;
-    }
-    addNumber(number) {
-        this.arrayOfNumbers.push(number);
-    }
-    sum() {
-        let total = 0;
-        this.arrayOfNumbers.forEach(elem => {
-            if (typeof elem === 'string' || typeof elem === 'number') {
-                total += this.getNumValue(elem);
-            }
-        });
-        return total;
-    }
-    getNumValue(val) {
-        if (typeof val === 'string') {
-            const num = parseInt(val);
-            return !isNaN(num) ? num : 0;
-        }
-        else {
-            return val;
-        }
-    }
+function initPage() {
+    const domElement = document.querySelector('.foo');
+    domElement.addEventListener('input', function (event) {
+        const e = event.target;
+        console.log(e.value);
+    });
 }
-const numbers1 = new Numbers(["test", "22"]);
-numbers1.addNumber("55");
-numbers1.addNumber("block");
-numbers1.addNumber("9");
-const numbers2 = new Numbers([23]);
-numbers2.addNumber(433);
-numbers2.addNumber(11);
-console.log(`1: ${numbers1.sum()}`);
-console.log(`2: ${numbers2.sum()}`);
