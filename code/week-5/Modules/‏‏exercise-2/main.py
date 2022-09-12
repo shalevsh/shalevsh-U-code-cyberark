90bc3c6968bf2362e373a7c6c92dc642f3a00266
shalev <shalevsharf@gmail.com>
2022-09-12 17:16:34 +0300

    ex2

---
+++ code/week-5/Modules/‏‏exercise-2/main.py
@@ -0,0 +1,14 @@
+# module1.py
+
+#__all__ = ["func1","func2"]
+def func1():
+    print("hello")
+    pass
+
+
+def func2():
+    print("world")
+    pass
+
+
+_num = 42  
---
+++ code/week-5/Modules/‏‏exercise-2/main2.py
@@ -0,0 +1,6 @@
+from main import *
+first_func = func1 
+second_func = func2 
+first_func()
+second_func()
+print(num)
