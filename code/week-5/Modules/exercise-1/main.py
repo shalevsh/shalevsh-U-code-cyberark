48648d5c11249b5f294baf960737db4aa85c83e0
shalev <shalevsharf@gmail.com>
2022-09-12 16:28:18 +0300

    ex1

---
+++ code/week-5/Modules/exercise-1/main.py
@@ -0,0 +1,10 @@
+# Import module calendar, and use its functions to write the following:
+# Find whether 2010 is a leap year. And 2020?
+# Find how many leap years there will be between 2020 - 2080 (inclusive).
+# Find which day of the week March 14, 2028 will be.
+import calendar
+year1 = 2010
+year2 = 2020
+# display the calendar
+print(calendar.isleap(year1))
+print(calendar.isleap(year2))
---
+++ code/week-5/exceptions/exercise-2/x.py
@@ -0,0 +1,3 @@
+# some_file.py
+if __name__ == "__main__":
+  print("This is the main script") 
