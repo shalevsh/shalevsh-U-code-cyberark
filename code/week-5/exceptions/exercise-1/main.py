8e388650645e33416ed7d9e17cb196ec983f9746
shalev <shalevsharf@gmail.com>
2022-09-12 11:22:40 +0300

    ex 1

---
+++ code/week-5/exceptions/exercise-1/main.py
@@ -0,0 +1,5 @@
+dividend = float(input("Enter the dividend: "))
+divisor = float(input("Enter the divisor: "))
+quotient = dividend/divisor
+print(math.floor(quotient)) 
+# divided by 0, value error
