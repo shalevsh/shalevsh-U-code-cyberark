5376fa99682eebb587920392a902781db041ea87
shalev <shalevsharf@gmail.com>
2022-09-18 12:24:21 +0300

    learn

--- code/week-5/Pytest Features/‏‏exercise-2/_test_store.py
+++ code/week-5/Pytest Features/‏‏exercise-2/_test_store.py
@@ -1,6 +1,26 @@
-from .primes import prime
+import pytest
 
 
-def test_is_three_prime():
-    assert prime.is_prime(3), "Test failed! the num isnt prime"
+@pytest.fixture
+def store():
+    items = ("apple", "banana", "orange")
+    amount = (2, 2, 1)
+    return dict(zip(items, amount))
 
+
+def buy_banana(store):
+    store["banana"] -= 1
+
+
+def buy_two_banana(store):
+    store["banana"] -= 2
+
+
+def test_buy_banana(store):
+    buy_banana(store)
+    assert store["banana"] == 1, "checks buy_banana function"
+
+
+def test_buy_two_bananas(store):
+    buy_two_banana(store)
+    assert store["banana"] == 0, "checks buy_two_banana function"
