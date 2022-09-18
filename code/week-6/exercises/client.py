e20c55a724a5051d1526a66b01be4da836a2cd8b
shalev <shalevsharf@gmail.com>
2022-09-18 15:58:16 +0300

    ex5

--- code/week-6/exercises/client.py
+++ code/week-6/exercises/client.py
@@ -1,7 +1,8 @@
 import requests
 
-priceObj = requests.get('http://localhost:8000/store/table')
+priceObj = requests.get('http://localhost:8000/store/chair')
 print(priceObj.json())
 
 itemObj = requests.get('http://localhost:8000/store/buy/chair')
-print(itemObj.json())
+item = itemObj.json()
+print(f"Congratulations, you've just bought{item}for {item['price']}. There are {item['inventory']} left now in the store")
