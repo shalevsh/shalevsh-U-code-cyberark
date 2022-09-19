251213db801c0b3ccd39f7895b13432d16fcda37
shalev <shalevsharf@gmail.com>
2022-09-19 10:23:53 +0300

    postman

--- code/week-6/FastAPI/exercises/client.py
+++ code/week-6/FastAPI/exercises/client.py
@@ -6,3 +6,6 @@ print(priceObj.json())
 itemObj = requests.get('http://localhost:8000/store/buy/chair')
 item = itemObj.json()
 print(f"Congratulations, you've just bought{item}for {item['price']}. There are {item['inventory']} left now in the store")
+
+storeObj = requests.get('http://localhost:8000/sale?admin=true')
+print(storeObj.json())
--- code/week-6/FastAPI/exercises/server.py
+++ code/week-6/FastAPI/exercises/server.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 import uvicorn
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-import requests
 
 store = [
     {"name": "table", "inventory": 3, "price": 800},
@@ -12,29 +11,13 @@ store = [
 ]
 
 app = FastAPI()
-
-# you were missing static folder (does not auto generate)
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
-@app.get("/")
-async def root():
-    return FileResponse('./static/download.jpeg')
-
-
-# @app.get("/book/{name}")
-# async def get_book(name):
-#     res = requests.get(
-#         'https://www.googleapis.com/books/v1/volumes?q={}'.format(name))
-#     return res.json()
-
 
 @app.get("/store/{name}")
 async def get_price(name):
     for s in store:
         if s['name'] == name:
             return {"price": s['price']}
-
     return {"price": None}
 
 
@@ -46,6 +29,16 @@ async def buy_item(name):
                 item['inventory'] -= 1
                 return item
     return {"error": "item not in stock"}
+
+@app.get("/sale")
+async def sale(adminSale):
+    if adminSale == "true":
+        for item in store:
+           if item['inventory'] > 10:
+                item['price'] /= 2
+                return store
+    else:
+        return store
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
