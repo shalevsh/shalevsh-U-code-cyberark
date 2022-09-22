001be095dd201f56004322ab040533220efe0a20
shalev <shalevsharf@gmail.com>
2022-09-22 12:26:23 +0300

    ex2

--- code/week-6/Basic authentication with FastAPI/server.py
+++ code/week-6/Basic authentication with FastAPI/server.py
@@ -6,18 +6,33 @@ from starlette.authentication import requires
 from fastapi_auth_middleware import AuthMiddleware, FastAPIUser
 
 # The method you have to provide
+
+
 def verify_authorization_header(auth_header: str) -> Tuple[List[str], FastAPIUser]:
-    user = FastAPIUser(first_name="Code", last_name="Specialist", user_id=1)  # Usually you would decode the JWT here and verify its signature to extract the 'sub'
-    scopes = ["admin"]  # You could for instance use the scopes provided in the JWT or request them by looking up the scopes with the 'sub' somewhere
+    # Usually you would decode the JWT here and verify its signature to extract the 'sub'
+    user = FastAPIUser(first_name="Code", last_name="Specialist", user_id=1)
+   # scopes = ["admin"]  # You could for instance use the scopes provided in the JWT or request them by looking up the scopes with the 'sub' somewhere
+    scopes = ["standard"]
     return scopes, user
 
+
 app = FastAPI()
-app.add_middleware(AuthMiddleware, verify_header=verify_authorization_header)  # Add the middleware with your verification method to the whole application
+# Add the middleware with your verification method to the whole application
+app.add_middleware(AuthMiddleware, verify_header=verify_authorization_header)
+
 
 @app.get('/')  # Sample endpoint (secured)
 @requires("admin")
 def home(request: Request):
     return request.user
 
+
+@app.get('/profile')  # Sample endpoint (secured)
+@requires("standard")
+def home(request: Request):
+    return request.user
+
+
 if __name__ == '__main__':
-    uvicorn.run('exercise_1:app', host="127.0.0.1", port=8000)  # Starts the uvicorn ASGI
+    uvicorn.run('exercise_1:app', host="127.0.0.1",
+                port=8000)  # Starts the uvicorn ASGI
